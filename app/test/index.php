574a6f9540f139162137aac6c794aba382678ff5
zeroonebeatz <zeroonebeatz@gmail.com>
2018-11-13 12:00:54 +0300

    fix(app/test): mysql connection

--- app/test/index.php
+++ app/test/index.php
@@ -4,12 +4,12 @@
  * Check db
  */
 
-$servername = "db";
+$servername = "mysql";
 $username = "root";
 $password = "local";
 
 try {
-    $conn = new \PDO("mysql:host=$servername;dbname=ttm", $username, $password);
+    $conn = new \PDO("mysql:host=$servername;dbname=test", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Mysql: Connected successfully";

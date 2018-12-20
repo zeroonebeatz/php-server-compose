ce413b0d20ae4fde8d445998396337b095a073b0
zeroonebeatz <zeroonebeatz@gmail.com>
2018-12-20 19:04:13 +0300

    fix(schedule, queue): use bash script instead of supervisor & cron

---
+++ docker/dockerfiles/php-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+while [ true ]
+do
+    php /var/www/ttm/artisan schedule:run --verbose --no-interaction
+    php /var/www/ttm/artisan queue:work --sleep=3 --tries=3 --timeout=60 &
+    sleep 60
+done

8e7962f601c9a1107cd14f9e57c593df65c4453f
zeroonebeatz <zeroonebeatz@gmail.com>
2018-12-21 15:29:34 +0300

    feat(schedule): add daemon

--- docker/dockerfiles/php-entrypoint.sh
+++ docker/dockerfiles/php-entrypoint.sh
@@ -6,5 +6,9 @@ while [ true ]
 do
     php /var/www/ttm/artisan schedule:run --verbose --no-interaction
     php /var/www/ttm/artisan queue:work --sleep=3 --tries=3 --timeout=60
+
+    php /var/www/idel/artisan schedule:run --verbose --no-interaction
+    php /var/www/idel/artisan queue:work --sleep=3 --tries=3 --timeout=60
+
     sleep 60
 done

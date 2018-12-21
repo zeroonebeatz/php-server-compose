4911d40f3b6ee286fcb7c24a0dcb83f51256d8ae
zeroonebeatz <zeroonebeatz@gmail.com>
2018-12-21 09:02:18 +0300

    fix: sheduler run command

--- docker/dockerfiles/php-entrypoint.sh
+++ docker/dockerfiles/php-entrypoint.sh
@@ -5,6 +5,6 @@ set -e
 while [ true ]
 do
     php /var/www/ttm/artisan schedule:run --verbose --no-interaction
-    php /var/www/ttm/artisan queue:work --sleep=3 --tries=3 --timeout=60 &
+    php /var/www/ttm/artisan queue:work --sleep=3 --tries=3 --timeout=60
     sleep 60
 done

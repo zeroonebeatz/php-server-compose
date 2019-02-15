987bf932c781edaea88d6be376826edc0a008590
zeroonebeatz <zeroonebeatz@gmail.com>
2019-02-15 17:05:49 +0300

    fix(php): schedule_opts run parallel

--- docker/dockerfiles/php/example.schedule_opts.sh
+++ docker/dockerfiles/php/example.schedule_opts.sh
@@ -4,8 +4,10 @@ set -e
 
 while [ true ]
 do
-    php /var/www/ttm/artisan schedule:run --verbose --no-interaction
-    php /var/www/ttm/artisan queue:work --sleep=3 --tries=3 --timeout=60
+    (
+        php /var/www/ttm/artisan schedule:run --verbose --no-interaction
+        php /var/www/ttm/artisan queue:work --sleep=3 --tries=3 --timeout=60
+    ) &
 
     sleep 60
 done
